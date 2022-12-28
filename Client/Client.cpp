9e1b3d33981d2a9f0af21a8d0967cb77b3586877
eagle <daniil.osockin@gmail.com>
2022-12-28 16:41:28 +0400

    Added guess value game

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -1,20 +1,48 @@
 #include "Common.hpp"
+#include <vector>
+#include <algorithm>
 
-int main()
+bool is_receive(int sock_fd, int& buf)
+{
+    int size = check(recv(sock_fd, &buf, sizeof(buf), MSG_WAITFORONE));
+    if (buf == -1)
+        return false;
+    check(size);
+    return true;
+}
+
+int main() 
 {
     auto dest_address = local_addr(SERVER_PORT);
-    int sock_fd = check(make_socket(SOCKET_TYPE));
+    int sock_fd = check(make_socket(SOCK_SEQPACKET));
     check(connect(sock_fd, (sockaddr*)&dest_address, sizeof(dest_address)));
 
-    std::string message;
-    while (true)
+    std::cout << "Connected to " << dest_address << std::endl;
+
+    std::vector<int> attempt = {};
+    for (int i = -1000; i <= 1000; attempt.push_back(i++));
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::shuffle(attempt.begin(), attempt.end(), mt);
+
+    int message = 0;
+    int buff;
+    int count = 0;
+    do
     {
-        std::getline(std::cin,  message);
-        if (message == "q")
+        message = attempt.back();
+        attempt.pop_back();
+
+        std::cout << "[" << ++count << "] " << message << std::endl;
+        check(send(sock_fd, &message, sizeof(message), MSG_WAITFORONE));
+
+        if(!is_receive(sock_fd, buff))
             break;
-        send(sock_fd, message.c_str(), message.size() + 1, MSG_WAITALL);
     }
+    while (buff != -1 || !(attempt.empty()));
 
     shutdown(sock_fd, SHUT_RDWR);
+    std::cout << "Disconnected from " << dest_address << std::endl;
+
     close(sock_fd);
 }
--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,37 +1,71 @@
 #include "Common.hpp"
 
-char buffer[64];
+#include <chrono>
+#include <sys/wait.h>
+
+typedef std::chrono::high_resolution_clock HRC;
+
+int buffer[64];
 
 int main()
 {
     auto server_address = local_addr(SERVER_PORT);
-    auto listening_socket = check(make_socket(SOCKET_TYPE));
+    auto listening_socket = check(make_socket(SOCK_SEQPACKET));
     int connected_socket = 0;
 
-    check(bind(listening_socket, (sockaddr*)&server_address, sizeof(server_address)));
+    check(bind(listening_socket, (sockaddr *) &server_address, sizeof(server_address)));
     check(listen(listening_socket, 2));
 
-    while (1)
+    while (true)
     {
         sockaddr_in connected_address{};
         socklen_t addr_len = sizeof(connected_socket);
-        connected_socket = check(accept(listening_socket, (sockaddr*)&connected_address,
-                                        &addr_len));
+
+        connected_socket = check(accept(listening_socket, (sockaddr *) &connected_address, &addr_len));
 
         std::cout << "Connected from " << connected_address << std::endl;
 
-        while (true)
+        static std::mt19937 gen(HRC::now().time_since_epoch().count());
+        std::uniform_int_distribution<> uid(-1000, 1000);
+        int guessed_value = uid(gen);
+        std::cout << "Guessed value = " << guessed_value << std::endl;
+
+        pid_t p = fork();
+
+        if (p)
         {
-            int size = recv(connected_socket, buffer, sizeof(buffer), MSG_WAITALL);
+            int count = 0;
+            while (true)
+            {
+                int tmp = 1;
 
-            if (size == 0 || (size < 0 && errno == ENOTCONN))
-                break; //disconnected
+                int st = 0;
+                if (waitpid(p, &st, WNOHANG) > 0)
+                {
+                    std::cout << "Terminating..." << std::endl;
+                    tmp = -1;
+                    check(send(connected_socket, &tmp, sizeof(int), MSG_WAITFORONE));
+                    break;
+                }
 
-            check(size);
-            std::cout   << connected_address
-                        << " send a message of a size " << size
-                        << ":" << std::string_view(buffer, size) << std::endl;
+                int size = check(recv(connected_socket, buffer, sizeof(buffer), MSG_WAITFORONE));
+                if (size == 0 || (size < 0 && errno == ENOTCONN))
+                    break; //disconnected
+
+                if (buffer[0] == guessed_value)
+                    tmp = -1;
+                else
+                    tmp = 0;
+                check(send(connected_socket, &tmp, sizeof(int), MSG_WAITFORONE));
+
+                check(size);
+                std::cout   << "[proc: " << p << "] ["
+                            << connected_address
+                            << "send a message of a size " << size
+                            << "] ---- [" << ++count << "] " << buffer[0] << std::endl;
+
+            }
+            std::cout << "Disconnected from " << connected_address << std::endl;
         }
-        std::cout << "Disconnected from " << connected_address << std::endl;
     }
 }
